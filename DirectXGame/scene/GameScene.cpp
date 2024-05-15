aaa55a27091186f5889206ac7b7abbf9f0e3a2c9
sasano <k023g0073@g.neec.ac.jp>
2024-05-15 13:05:57 +0900

    2-27ページまで完了

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -4,13 +4,21 @@
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+	delete model_;
+	for (WorldTransform* worldTransformBlock : worldTransformBlocks_) {
+		delete worldTransformBlock;
+	}
+	worldTransformBlocks_.clear();
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+
+	model_ = Model::Create();
 }
 
 void GameScene::Update() {}
--- DirectXGame/scene/GameScene.h
+++ DirectXGame/scene/GameScene.h
@@ -7,6 +7,7 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
+#include<vector>
 
 /// <summary>
 /// ゲームシーン
@@ -38,6 +39,11 @@ public: // メンバ関数
 	/// 描画
 	/// </summary>
 	void Draw();
+
+	//3Dモデル
+	Model* model_ = nullptr;
+
+	std::vector<WorldTransform*> worldTransformBlocks_;
 
 private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
